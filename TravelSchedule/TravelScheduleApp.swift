24ed74857d7b584d31133bc8b5de3a4a5351f286
Ann@ <ann-brown956@yandex.ru>
2024-12-06 13:31:43 +0300

    Initial Commit

---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Анна Браун on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelScheduleTests/TravelScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleTests.swift
+//  TravelScheduleTests
+//
+//  Created by Анна Браун on 06.12.2024.
+//
+
+import Testing
+@testable import TravelSchedule
+
+struct TravelScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
